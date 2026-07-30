b43b05b3740bd71719ce7f5185a2e7435fc251a0
aymen <aymenfkir23@gmail.com>
2026-07-30 22:26:33 +0100

    updated pre commit

--- client/client_example/src/client_example/main.py
+++ client/client_example/src/client_example/main.py
@@ -1,2 +1,6 @@
 def func(msg: str) -> str:
     return msg
+
+
+def multiply(a: int, b: int) -> int:
+    return a * b
--- client/client_example/test/test_main.py
+++ client/client_example/test/test_main.py
@@ -1,6 +1,10 @@
 # create a test that test main function in src/client_example/main.py
-from src.client_example.main import func
+from src.client_example.main import func, multiply
 
 
 def test_func() -> None:
     assert func("hello") == "hello"
+
+
+def test_multiply() -> None:
+    assert multiply(2, 3) == 6
